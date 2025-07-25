f0c13b5a86011fedafddbce6b440a5c876cf6452
Ulika <you203332@mail.ru>
2025-07-25 03:49:38 +0300

    new file App

---
+++ App.vue
@@ -0,0 +1,14 @@
+<script>
+  import Counter from './counter.vue';
+import counter from './counter.vue';
+
+  export default{
+    components: {
+        counter
+    }
+  }
+</script>
+
+<template>
+  <Counter />
+</template>
---
+++ counter.vue
@@ -0,0 +1,17 @@
+<script>
+  export default {
+    data() {
+        return {count: 0}
+    },
+    methods: {
+        inc() {this.count++}
+    }
+  }
+</script>
+
+<template>
+
+    <h1>{{ count }}</h1>
+    <button @click="inc">+1</button>
+</template>
+
